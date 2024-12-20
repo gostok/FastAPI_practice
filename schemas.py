eba2b497f742656f13e405b2ecaa338d1dff88d5
gostok <stiv4ik12012@gmail.com>
2024-12-20 09:38:32 +0300

    model_config

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional, Annotated
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class STaskAdd(BaseModel):
@@ -9,6 +9,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class STaskID(BaseModel):
